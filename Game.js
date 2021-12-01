ebb8d7989c3b533f3a3d95ebe116c92f60d3d1af
KrouTm <caroline-ti@hotmail.com>
2021-12-01 17:20:43 +0000

    up

--- Game.js
+++ Game.js
@@ -6,113 +6,78 @@ const user_0= document.getElementById("user-0");
 const pc_0= document.getElementById("pc-0");
 const board_n= document.querySelector(".board");
 const result_= document.querySelector(".result > p");
-const rock= document.getElementById("r");
-const paper= document.getElementById("p");
-const scissors= document.getElementById("s");
-const lizard= document.getElementById("l");
-const spock= document.getElementById("e");
+const rock= document.getElementById("Rock");
+const paper= document.getElementById("Paper");
+const scissors= document.getElementById("Scissors");
+const lizard= document.getElementById("Lizard");
+const spock= document.getElementById("Spock");
 const three_= document.getElementById("three")
 const five_= document.getElementById("five")
 const infinity_= document.getElementById("infinity")
 
-function get_pcchoice(){
-    const choices= ["r","p","s","l","e"];
-    const random_n= (Math.floor(Math.random()*4));
-    return choices[random_n];
-}
+const playMatch = () => {
+const choices = document.querySelectorAll(".choices > i");
+const userhand = document.querySelector(".user-hand");
+const pchand = document.querySelector(".pc-hand");
+const hands = document.querySelectorAll(".center-hands img");
 
-function transform(letter){
-    if(letter==="r") return "Rock";
-    if(letter==="p") return "Paper";
-    if(letter==="s") return "Scissors";
-    if(letter==="l") return "Lizard";
-    if(letter==="e") return "Spock";
-}
+hands.forEach(hand => {hand.addEventListener("animationend", function() {this.style.animation = "";});});
+const choices= ["Rock","Paper","Scissors","Lizard","Spock"];
+choices.forEach(choices => {
+    choices.addEventListener("click", function() {
+        const random_n= (Math.floor(Math.random()*5));
+        const pcchoice = choices[random_n];
 
-function win(userchoice, pcchoice){
-    userpoint++;
-    user_0.innerHTML= userpoint;
-    pc_0.innerHTML= pcpoint;
-    result_.innerHTML= "Your "+transform(userchoice)+" beats the PC "+transform(pcchoice)+". You WIN!";
-}
-
-function lose(userchoice, pcchoice){
-    pcpoint++;
-    user_0.innerHTML= userpoint;
-    pc_0.innerHTML= pcpoint;
-    result_.innerHTML= "The PC "+transform(pcchoice)+" beats your "+transform(userchoice)+". You LOST!";
-}
-
-function draw(userchoice, pcchoice){
-    user_0.innerHTML= userpoint;
-    pc_0.innerHTML= pcpoint;
-    result_.innerHTML= "The PC "+transform(pcchoice)+" equals your "+transform(userchoice)+". It's a DRAW!";
-}
-
-function play(userchoice){
+    setTimeout(() => {
+        compareHands(this.textContent, pcchoice);
+        userhand.src = `${this.textContent}.png`;
+        pchand.src = `${pcchoice}.png`;
+    }, 2000);
+    userhand.style.animation = "shakeUser 2s ease";
+    pchand.style.animation = "shakePC 2s ease";
+    });
+});
+};
+function n_chances(){
     let mylimit = sessionStorage.getItem('mylimit');
-    console.log(mylimit);
     if (n_matches < mylimit) {
-    const pcchoice= get_pcchoice();
-    switch (userchoice + pcchoice){
-        case "rs":
-        case "rl":
-        case "pr":
-        case "pe":
-        case "sp":
-        case "sl":
-        case "lp":
-        case "le":
-        case "es":
-        case "er":
-            win(userchoice, pcchoice);
-            n_matches++;
-            break;
-        case "rp":
-        case "re":
-        case "ps":
-        case "pl":
-        case "se":
-        case "sr":
-        case "lr":
-        case "ls":
-        case "ep":
-        case "el":
-            lose(userchoice, pcchoice);
-            n_matches++;
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-        case "ll":
-        case "ee":
-            draw(userchoice, pcchoice);
-            break;
+}
+const compareHands = (userchoice, pcchoice) => {
+    if(userchoice === pcchoice){result_.textContent="It's a DRAW!"; return;}
+    if(userchoice === "Rock"){n_matches++;
+        if(pcchoice === "Scissors"); else if(pcchoice === "Lizard"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
     }
+    if(userchoice === "Paper"){n_matches++;
+        if(pcchoice === "Rock"); else if(pcchoice === "Spock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
+    }
+    if(userchoice === "Scissors"){n_matches++;
+        if(pcchoice === "Paper"); else if(pcchoice === "Lizard"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
+    }
+    if(userchoice === "Lizard"){n_matches++;
+        if(pcchoice === "Paper"); else if(pcchoice === "Spock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
+    }
+    if(userchoice === "Spock"){n_matches++;
+        if(pcchoice === "Scissors"); else if(pcchoice === "Rock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
     }
 }
-
 function main(){
-    rock.addEventListener('click', function(){play("r");})
-    paper.addEventListener('click', function(){play("p");})
-    scissors.addEventListener('click', function(){play("s");})
-    lizard.addEventListener('click', function(){play("l");})
-    spock.addEventListener('click', function(){play("e");})
+    rock.addEventListener('click', function(){compareHands("Rock");})
+    paper.addEventListener('click', function(){compareHands("Paper");})
+    scissors.addEventListener('click', function(){compareHands("Scissors");})
+    lizard.addEventListener('click', function(){compareHands("Lizard");})
+    spock.addEventListener('click', function(){compareHands("Spock");})
 }
 try {choose_matches();}
 catch(err) {main();}
 
 function choose_matches(){
-    three_.addEventListener('click', function(){limit=3; sessionStorage.setItem('mylimit', limit); console.log('eh ', limit)})
-    five_.addEventListener('click', function(){limit=5; sessionStorage.setItem('mylimit', limit); console.log('eh ', limit)})
-    infinity_.addEventListener('click', function(){limit=9999; sessionStorage.setItem('mylimit', limit); console.log('eh infinito!', limit)})
+    three_.addEventListener('click', function(){limit=3; sessionStorage.setItem('mylimit', limit);})
+    five_.addEventListener('click', function(){limit=5; sessionStorage.setItem('mylimit', limit);})
+    infinity_.addEventListener('click', function(){limit=9999; sessionStorage.setItem('mylimit', limit);})
 }
-
-
-
-
-
-
+playMatch()
+main()
+choose_matches()
 
 
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Style
 I made the game with rgb colors to bring to it some kind of funny.
 The black background stands out more like cores and makes it more comfortable for the eyes.
 
+
