fc40a8880129e71d33d13e79cb6e1a3e63401cb8
KrouTm <caroline-ti@hotmail.com>
2021-12-06 14:16:18 +0000

    updated

--- Game.js
+++ Game.js
@@ -2,82 +2,84 @@ let userpoint= 0;
 let pcpoint= 0;
 let n_matches= 0;
 let limit= 0;
-const user_0= document.getElementById("user-0");
-const pc_0= document.getElementById("pc-0");
-const board_n= document.querySelector(".board");
+const userpoint_0= document.getElementById("user-0")
+const pcpoint_0= document.getElementById("pc-0")
 const result_= document.querySelector(".result > p");
 const rock= document.getElementById("Rock");
 const paper= document.getElementById("Paper");
 const scissors= document.getElementById("Scissors");
 const lizard= document.getElementById("Lizard");
 const spock= document.getElementById("Spock");
-const three_= document.getElementById("three")
-const five_= document.getElementById("five")
-const infinity_= document.getElementById("infinity")
+const three_= document.getElementById("three");
+const five_= document.getElementById("five");
+const infinity_= document.getElementById("infinity");
 
-const playMatch = () => {
-const choices = document.querySelectorAll(".choices > i");
-const userhand = document.querySelector(".user-hand");
-const pchand = document.querySelector(".pc-hand");
-const hands = document.querySelectorAll(".center-hands img");
-
-hands.forEach(hand => {hand.addEventListener("animationend", function() {this.style.animation = "";});});
-const choices= ["Rock","Paper","Scissors","Lizard","Spock"];
-choices.forEach(choices => {
-    choices.addEventListener("click", function() {
-        const random_n= (Math.floor(Math.random()*5));
-        const pcchoice = choices[random_n];
-
-    setTimeout(() => {
-        compareHands(this.textContent, pcchoice);
-        userhand.src = `${this.textContent}.png`;
-        pchand.src = `${pcchoice}.png`;
-    }, 2000);
-    userhand.style.animation = "shakeUser 2s ease";
-    pchand.style.animation = "shakePC 2s ease";
-    });
-});
-};
-function n_chances(){
+function playMatch() {
+    const choices = document.querySelectorAll(".choices i");
+    const userhand = document.querySelector(".user-hand");
+    const pchand = document.querySelector(".pc-hand");
+    const hands = document.querySelectorAll(".center-hands img");
+    hands.forEach(hand => {hand.addEventListener("animationend", function() {this.style.animation = "";});});
+    const choicespc = ["Rock","Paper","Scissors","Lizard","Spock"];
     let mylimit = sessionStorage.getItem('mylimit');
-    if (n_matches < mylimit) {
+
+    choices.forEach(choice => {
+        choice.addEventListener("click", function() {
+            console.log(mylimit)
+            console.log(n_matches)
+            if (n_matches < mylimit){
+                const random_n= (Math.floor(Math.random()*5));
+                const pcchoice = choicespc[random_n];
+                setTimeout(() => {
+                    console.log(this.id)
+                    compareHands(this.id, pcchoice);
+                    userhand.src = `${this.id}.png`;
+                    pchand.src = `${pcchoice}.png`;
+                }, 2000);
+                userhand.style.animation = "shakeUser 2s ease";
+                pchand.style.animation = "shakePC 2s ease";
+            }
+            else {console.log("cabo suas chance!")}
+        });
+    });
+};
+
+function updateUser(){
+    userpoint++;
+    userpoint_0.innerHTML= userpoint;
+};
+function updatePC(){
+    pcpoint++;
+    pcpoint_0.innerHTML= pcpoint;
+};
+
+function pUser(){
+    result_.textContent="ola"
 }
+
+function pPC(){
+    result_.textContent="sla"
+}
+
 const compareHands = (userchoice, pcchoice) => {
-    if(userchoice === pcchoice){result_.textContent="It's a DRAW!"; return;}
-    if(userchoice === "Rock"){n_matches++;
-        if(pcchoice === "Scissors"); else if(pcchoice === "Lizard"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
-    }
-    if(userchoice === "Paper"){n_matches++;
-        if(pcchoice === "Rock"); else if(pcchoice === "Spock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
-    }
-    if(userchoice === "Scissors"){n_matches++;
-        if(pcchoice === "Paper"); else if(pcchoice === "Lizard"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
-    }
-    if(userchoice === "Lizard"){n_matches++;
-        if(pcchoice === "Paper"); else if(pcchoice === "Spock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
-    }
-    if(userchoice === "Spock"){n_matches++;
-        if(pcchoice === "Scissors"); else if(pcchoice === "Rock"){result_.textContent="You WIN!"; userpoint++; return;} else{result_.textContent="Pc Wins!"; pcpoint++; return;}
-    }
+    if(userchoice === pcchoice){result_.textContent="It's a DRAW!";}
+    else if(userchoice === "Rock"){n_matches++;
+        if(pcchoice === "Scissors" || pcchoice === "Lizard"){pUser(); updateUser();} else{pPC(); updatePC();}}
+    else if(userchoice === "Paper"){n_matches++;
+        if(pcchoice === "Rock" || pcchoice === "Spock"){pUser(); updateUser();} else{pPC(); updatePC();}}
+    else if(userchoice === "Scissors"){n_matches++;
+        if(pcchoice === "Paper" || pcchoice === "Lizard"){pUser(); updateUser();} else{pPC(); updatePC();}}
+    else if(userchoice === "Lizard"){n_matches++;
+        if(pcchoice === "Paper" || pcchoice === "Spock"){pUser(); updateUser();} else{pPC(); updatePC();}}
+    else if(userchoice === "Spock"){n_matches++;
+        if(pcchoice === "Scissors" || pcchoice === "Rock"){pUser(); updateUser();} else{pPC(); updatePC();}}
 }
-function main(){
-    rock.addEventListener('click', function(){compareHands("Rock");})
-    paper.addEventListener('click', function(){compareHands("Paper");})
-    scissors.addEventListener('click', function(){compareHands("Scissors");})
-    lizard.addEventListener('click', function(){compareHands("Lizard");})
-    spock.addEventListener('click', function(){compareHands("Spock");})
-}
-try {choose_matches();}
-catch(err) {main();}
 
 function choose_matches(){
     three_.addEventListener('click', function(){limit=3; sessionStorage.setItem('mylimit', limit);})
     five_.addEventListener('click', function(){limit=5; sessionStorage.setItem('mylimit', limit);})
     infinity_.addEventListener('click', function(){limit=9999; sessionStorage.setItem('mylimit', limit);})
 }
-playMatch()
-main()
-choose_matches()
 
-
-
+try {choose_matches();}
+catch(err) {playMatch();}
--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # RockPaperScissorsLizardSpock
  RockPaperScissorsLizardSpock Project
 
-Style
-I made the game with rgb colors to bring to it some kind of funny.
-The black background stands out more like cores and makes it more comfortable for the eyes.
+HTML
+Esta parte do websit e bem simples pois nao havia nescessidade de complicacao. 
+- Board Score
+para marcar o numero de pontos do usuario e do pc de forma objetiva e facil de entender.
+- Hands
+foi incluido uma imagem de maos fechadas simulando a introducao do jogo assim como na vida real.
+- Options
+foi colocado mais duas opcoes de jogada alem do tradicional pedra papel tesura, para tornar o joog menos limitado.
+-Reset
+para que o usuario tenha a facilidade em recomecar o jogo quantas vezes e no momento que desejar.
 
+CSS
+- ao contrario do Html, nao economizei recursos para dar estilo ao website.
+- o design todo do jogo foi inspirado no mickey e suas cores classicas.
+- ferramentas como Krita foram usados para fazer o desenho das maos nos formatos que necessitava para o desenvolvimento do jogo.
+- apesar de mtos detalhes escritos no codigo, nao interferiu na simplicidade do website, sendo esta a intencao desde o comeco.
+- em todos os icones que foram feitos para ser clicados, foi usado o recurso do a:hover para que o usuario tenha certeza que o icone e uma opcao de click.
 
+JavaScript
+- o primeiro dilema foi como tornaria o site menos estatico e entediante, pois o jogo deveria ser pratico e ao mesmo tempo interessante visualmente, criar uma movimentacao seria necessario para isso.
+- apos declarar todas as variaveis que seria necessarias para dar ordens a elas, foram feitas as opcoes de escolha "melhor de tres", "melhor de 5" e "infinito", a ultima opcao foi deixada apenas para testores, pois nao seria apropriado ter um placar maior que 2 digitos para cada jogador.
